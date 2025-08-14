19d2b8016643f486a74955919c316feb5951fdb9
phan-forsure <phantommy38@gmail.com>
2025-08-14 07:20:25 +0300

    removed boilerplate

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,4 +1,4 @@
-import { app, shell, BrowserWindow, ipcMain } from 'electron'
+import { app, shell, BrowserWindow } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
@@ -49,8 +49,8 @@ app.whenReady().then(() => {
     optimizer.watchWindowShortcuts(window)
   })
 
-  // IPC test
-  ipcMain.on('ping', () => console.log('pong'))
+  // // IPC test
+  // ipcMain.on('ping', () => console.log('pong'))
 
   createWindow()
 
