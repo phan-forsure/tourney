7ad9e817eceac668936511fc649ad3b98a20a534
phan-forsure <phantommy38@gmail.com>
2025-08-28 07:39:42 +0300

    added icons

--- src/renderer/src/comps/MainPage.tsx
+++ src/renderer/src/comps/MainPage.tsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 import Notifications from './Notifications'
 import CancerDetection from './screens/CancerDetection'
 import DietPlan from './pages/DietPlan'
+import { Activity, Salad } from 'lucide-react'
 
 const componentClass =
-  'p-12 m-4! bg-[var(--color-background-soft)] opacity-80 cursor-pointer transition-all flex justify-center items-center text-2xl rounded-xl border-1 border-[var(--color-background-dark)] w-90 h-64 hover:opacity-100 hover:shadow-lg'
+  'p-12 m-4! bg-[var(--color-background-soft)] opacity-80 cursor-pointer transition-all flex flex-col justify-center items-center text-2xl rounded-xl border-1 border-[var(--color-background-dark)] w-90 h-64 hover:opacity-100 hover:shadow-lg gap-4'
 
 export default function MainPage(): React.JSX.Element {
   const [activeContent, setActiveContent] = useState<'none' | 'cancer' | 'diet'>('none')
@@ -12,16 +13,22 @@ export default function MainPage(): React.JSX.Element {
   return (
     <div className="main-screen overflow-y-scroll min-2xl:mx-60! justify-center flex h-full flex-wrap">
       <div
-        className={`cancer-detection ${componentClass} ${activeContent === 'cancer' ? 'bg-green-100 !opacity-100' : ''}`}
+        className={`cancer-detection ${componentClass} ${
+          activeContent === 'cancer' ? 'bg-green-100 !opacity-100' : ''
+        }`}
         onClick={() => setActiveContent((current) => (current === 'cancer' ? 'none' : 'cancer'))}
       >
-        Cancer Detection
+        <Activity size={40} />
+        <span>Cancer Detection</span>
       </div>
       <div
-        className={`diet ${componentClass} ${activeContent === 'diet' ? 'bg-green-100 !opacity-100' : ''}`}
+        className={`diet ${componentClass} ${
+          activeContent === 'diet' ? 'bg-green-100 !opacity-100' : ''
+        }`}
         onClick={() => setActiveContent((current) => (current === 'diet' ? 'none' : 'diet'))}
       >
-        Diet Plan
+        <Salad size={40} />
+        <span>Diet Plan</span>
       </div>
       {activeContent !== 'none' ? (
         <div className="w-full m-4! p-6 bg-white rounded-xl shadow-sm flex justify-center">
